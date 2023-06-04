cfe307c177a4d420430c1fdf68c5244ee9aec4d9
shuyu liu <liushuyu6666@gmail.com>
2023-06-04 01:24:05 -0400

    add Points in utils folder

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ This a starter project for React.js.
 # How to Use
 1. Install `node.js`.
 2. run `yarn` to install all packages.
+
+
+# Canvas Grid Coordination
+<img src="static/canvas_grid.png" alt="Canvas Grid" weight='150px' height='150px'>
---
+++ src/utils/Points.ts
@@ -0,0 +1,67 @@
+import { Coordination } from '../components/Coordination';
+
+export interface GridArea {
+    rowStart: number;
+    rowEnd: number;
+    columnStart: number;
+    columnEnd: number;
+}
+
+export interface GridTemplate {
+    gridTemplateRows: number;
+    gridTemplateColumns: number;
+}
+
+export class Points {
+    points: Coordination[];
+    gridArea: GridArea | undefined;
+    gridTemplate: GridTemplate | undefined;
+
+    constructor() {
+        this.points = [];
+        this.gridArea = undefined;
+        this.gridTemplate = undefined;
+    }
+
+    public addPoint(point: Coordination) {
+        this.points.push(point);
+
+        if (!this.gridArea || !this.gridTemplate) {
+            // Update girdArea.
+            this.gridArea = {
+                rowStart: point.x,
+                rowEnd: point.x,
+                columnStart: point.y,
+                columnEnd: point.y,
+            };
+
+            // Update gridTemplate.
+            this.gridTemplate = {
+                gridTemplateRows: 0,
+                gridTemplateColumns: 0,
+            };
+        } else {
+            // Update girdArea.
+            if (point.x < this.gridArea.rowStart) {
+                this.gridArea.rowStart = point.x;
+            }
+            if (point.x > this.gridArea.rowEnd) {
+                this.gridArea.rowEnd = point.x;
+            }
+            if (point.y < this.gridArea.columnStart) {
+                this.gridArea.columnStart = point.y;
+            }
+            if (point.y > this.gridArea.columnEnd) {
+                this.gridArea.columnEnd = point.y;
+            }
+
+            // Update gridTemplate.
+            this.gridTemplate.gridTemplateRows = Math.abs(
+                this.gridArea!.rowEnd - this.gridArea!.rowStart,
+            );
+            this.gridTemplate.gridTemplateColumns = Math.abs(
+                this.gridArea!.columnEnd - this.gridArea!.columnStart,
+            );
+        }
+    }
+}
